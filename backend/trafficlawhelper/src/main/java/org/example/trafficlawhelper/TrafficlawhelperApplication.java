cc06e2ae540667b49dee31a0b9fa904a89eb1a35
Jinseong <hdss8234@gmail.com>
2024-07-13 21:21:51 +0900

    feat: 프로젝트 초기 설정

---
+++ backend/trafficlawhelper/src/main/java/org/example/trafficlawhelper/TrafficlawhelperApplication.java
@@ -0,0 +1,13 @@
+package org.example.trafficlawhelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrafficlawhelperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrafficlawhelperApplication.class, args);
+    }
+
+}
