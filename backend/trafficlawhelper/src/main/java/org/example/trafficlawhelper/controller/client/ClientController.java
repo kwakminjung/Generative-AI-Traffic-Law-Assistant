99f5ba2d9d258d82b4ecfcd3bbbb139b692400a7
Jinseong <hdss8234@gmail.com>
2024-07-16 15:18:07 +0900

    feat : 요청 및 응답 API 구현

--- backend/trafficlawhelper/src/main/java/org/example/trafficlawhelper/controller/client/ClientController.java
+++ backend/trafficlawhelper/src/main/java/org/example/trafficlawhelper/controller/client/ClientController.java
@@ -1,24 +1,24 @@
 package org.example.trafficlawhelper.controller.client;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.ai.chat.client.ChatClient;
+import org.example.trafficlawhelper.service.client.ChatClientService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/ai")
 public class ClientController {
 
-    private final ChatClient chatClient;
+    private final ChatClientService chatClientService;
 
     @GetMapping
-    public String chat(@RequestParam(defaultValue = "안녕하세요.") String userMessage) {
-        return this.chatClient.prompt()
-                .user(userMessage)
-                .call()
-                .content();
+    public Map<String, String> chat(@RequestParam String userMessage) {
+        String response = chatClientService.generate(userMessage);
+        return Map.of("response", response);
     }
 }
